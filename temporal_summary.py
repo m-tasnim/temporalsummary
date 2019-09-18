2d3d56eba280dca263b15e17f2837b9b63b21484
m-tasnim <mayesha.tasnim@gmail.com>
2019-09-18 13:52:54 +0200

    Fixed bug + added triple counting stats

--- temporal_summary.py
+++ temporal_summary.py
@@ -1,5 +1,6 @@
 from rdflib import Graph, URIRef, Literal
 from random import sample
+import numpy as np
 from concepts import Context
 
 class TemporalSummary:
@@ -10,11 +11,15 @@ class TemporalSummary:
     def __init__(self, subjects_path, graphs_paths):
         self.graphs = []
         self.subjects = []
+        self.triples = 0
+        self.summarized_triples = 0
         with open(subjects_path) as fp:
-            self.subjects.append(fp.readline().strip())
+            for line in fp:
+                self.subjects.append(line.strip())
         for graph_path in graphs_paths:
             graph = Graph()
             graph.parse(graph_path, format='turtle')
+            self.triples += sum(1 for x in graph.triples((None, None, None)))
             self.graphs.append(graph)
 
     def summarize(self):
@@ -22,23 +27,28 @@ class TemporalSummary:
             molecules = self.get_molecules(subject)
             fca_result = self.local_fca(molecules)
             summary = self.fca_summarization(fca_result)
-            for ((mlist, plist)) in summary:
-                print(mlist, plist)
-                # print("extent:{}\nintent:{}".format(extent, intent))
+            self.summarized_triples += len(summary)
+            # for (mlist, plist) in summary:
+            #     print(mlist, plist)
+
+        print("Original triples: {}".format(self.triples) )
+        print("Summarized triples: {}".format(self.summarized_triples))
+        print("Compression: {}".format(self.triples - self.summarized_triples))
 
     def fca_summarization(self, fca_result):
         summary = []
-        for mlist, plist in fca_result:
-            if len(mlist) > 1 and len(plist) >=0:
+        intents = [plist for (mlist, plist) in fca_result]
+        sorted_intents = lists = [i for i, _ in (sorted(enumerate(intents), key=lambda x: len(x[1])))]
+
+        for index in sorted_intents:
+            mlist, plist = fca_result[index]
+            if len(mlist) > 0 and len(plist) > 0:
                 summary.append((mlist, plist))
-                fca_result.remove((mlist, plist))
-        for mlist, plist in fca_result:
-            if len(mlist) == 1 and len(plist) >=0:
-                for (x, y) in summary:
-                    if mlist[0] in x and y in plist:
-                        plist.remove(y)
-                if len(plist) >= 0:
-                    summary.append((mlist, plist))
+            if len(mlist) > 1:
+                for i in range(len(fca_result)):
+                    x, y = fca_result[i]
+                    if i != index:
+                        fca_result[i] = (x, list(set(y) - set(plist)))
         return summary
 
     def get_molecules(self, subject):
@@ -47,10 +57,6 @@ class TemporalSummary:
             molecule = Graph()
             molecule += graph.triples((URIRef(subject), None, None))
             molecules.append(molecule)
-        # for molecule in molecules:
-        #     print("molecule:\n")
-        #     for s, p, o in molecule:
-        #         print ("<{}><{}><{}>\n".format(s, p, o))
         return molecules
 
     def get_properties(self, molecules):
@@ -62,10 +68,11 @@ class TemporalSummary:
 
     def local_fca(self, molecules):
         props = self.get_properties(molecules)
+        props = list(set(props))
         sub = list(molecules[0].subjects())[0]
 
-        molecule_properties = [str(prop) + '->' + str(value) for prop, value in props]
-        molecule_names = ["{}_{}".format(str(sub), y) for y in [2014, 2015, 2016]]
+        molecule_properties = [str(prop.encode('utf-8')) + '->' + str(value.encode('utf-8')) for prop, value in props]
+        molecule_names = ["{}_{}".format(str(sub.encode('utf-8')), y) for y in [2014, 2015, 2016]]
 
         mat = []
         for molecule in molecules:
@@ -75,9 +82,6 @@ class TemporalSummary:
                     row[idx] = True
             mat.append(row)
 
-        print(molecule_names)
-        print(molecule_properties)
-        print(mat)
         c = Context(molecule_names, molecule_properties, mat)
         res = c.lattice
         result = []
@@ -87,8 +91,8 @@ class TemporalSummary:
         return result
 
 
-graphs_paths = ["data/shortened/person{}_10_test.nt".format(year) for year in [2014, 2015, 2016]]
-summarization = TemporalSummary("data/subjects_test.txt", graphs_paths)
+graphs_paths = ["data/shortened/person{}_10.nt".format(year) for year in [2014, 2015, 2016]]
+summarization = TemporalSummary("data/subjects.txt", graphs_paths)
 summarization.summarize()
 
 
